9e3f7fc89d9928e4d8e7971fcc3ebff30268b0ce
Zach <djessitokoto@gmail.com>
2023-08-05 18:56:32 +0200

    mongo init

--- backend/App.js
+++ backend/App.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://JonhC:chocolat@cluster0.7afshel.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
 
 
@@ -32,15 +39,6 @@ function login(req, res, next) {
   }; // Middleware POST pour connecter un utilisateur
 
 
-
-
-
-
-
-
-
-
-   // Middleware POST pour créer un objet
 
 
 
