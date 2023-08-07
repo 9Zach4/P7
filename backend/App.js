1350f0cb4afbc817ad368d729836a185c5b5f3d9
Zach <djessitokoto@gmail.com>
2023-08-07 14:11:42 +0200

    sign up et login routes

--- backend/App.js
+++ backend/App.js
@@ -1,32 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
+
 mongoose.connect('mongodb+srv://JonhC:chocolat@cluster0.7afshel.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  // importation des routes d'authentification et de gestion des livres
 const bookRoutes = require('./routes/book');
-
+const userRoutes = require('./routes/user');
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  }); // Middleware général appliqué à toutes les requêtes envoyées au serveur
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+}); // Middleware général appliqué à toutes les requêtes envoyées au serveur
 
 
 app.use(express.json());
 
 app.use('/api/books', bookRoutes)
-
-
-
-
- 
-
+app.use('/api/auth', userRoutes)
 
 module.exports = app;
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const bookCrtl = require('../controllers/book');
 
-router.post('/', bookCrtl.createBook );
+router.post('/', bookCrtl.createBook ); 
 router.put('/:id',bookCrtl.modifyBook);
 router.delete('/:id', bookCrtl.deleteBook);
 router.get('/:id', bookCrtl.getOneBook);
